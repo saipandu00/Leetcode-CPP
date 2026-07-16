9bd9374d5173f1ae2fbfde09666e4130d512f7b1
Vamshi krishna2005 <149609715+saipandu00@users.noreply.github.com>
2026-07-16 11:56:13 +0530

    Time: 0 ms (100%), Space: 18.8 MB (5.67%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty()) {
+            TreeNode* n1 = q.front(); q.pop();
+            TreeNode* n2 = q.front(); q.pop();
+            if (!n1 && !n2) continue;
+            if (!n1 || !n2 || n1->val != n2->val)
+                return false;
+            q.push(n1->left);
+            q.push(n2->right);
+            q.push(n1->right);
+            q.push(n2->left);
+        }
+        return true;
+    }
+};
+//Push root->left and root->right into a queue.
+// While the queue is not empty:
+// Pop two nodes.
+// If both are nullptr, continue.
+// If one is nullptr or their values are different, return false.
+// Push their children in mirror order:
+// left->left and right->right
+// left->right and right->left
